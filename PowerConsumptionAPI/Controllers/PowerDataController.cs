81dd7952e7ef8b1f533688292f791137be5e8ba9
Astijus Arimavičius <astijus.arimavicius@gmail.com>
2023-02-14 15:56:51 +0200

    Remove examples and add power data controller

---
+++ PowerConsumptionAPI/Controllers/PowerDataController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PowerConsumptionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PowerDataController : ControllerBase
+    {
+
+    }
+}
