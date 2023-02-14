5a6a05ea19bd0f2ad65ec0d10a2e16612c175214
Astijus Arimavičius <astijus.arimavicius@gmail.com>
2023-02-14 20:34:58 +0200

    Add computer controller

---
+++ PowerConsumptionAPI/Controllers/ComputerController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PowerConsumptionAPI.Filters.ActionFilters;
+using PowerConsumptionAPI.Models;
+using PowerConsumptionAPI.Models.DTOs.Computer;
+
+namespace PowerConsumptionAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComputerController : ControllerBase
+    {
+        private readonly RepositoryContext _repositoryContext;
+        private readonly ILogger<ComputerController> _logger;
+        private readonly IMapper _mapper;
+
+        public ComputerController(RepositoryContext repositoryContext, ILogger<ComputerController> logger, IMapper mapper)
+        {
+            _repositoryContext = repositoryContext;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetComputers()
+        {
+            var computers = await _repositoryContext.Computers
+                .AsNoTracking()
+                .ToListAsync();
+
+            var computersDto = _mapper.Map<IEnumerable<ComputerDto>>(computers);
+
+            return Ok(computersDto);
+        }
+
+        [HttpGet("{computerId}")]
+        public async Task<IActionResult> GetComputer(string computerId)
+        {
+            var computer = await _repositoryContext.Computers
+                .SingleOrDefaultAsync(c => c.Id == computerId);
+
+            if (computer == null)
+            {
+                _logger.LogWarning($"Computer with id: {computerId} does not exist in the database.");
+                return NotFound();
+            }
+
+            var computerDto = _mapper.Map<ComputerDto>(computer);
+
+            return Ok(computerDto);
+        }
+
+        [HttpPost]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        public async Task<IActionResult> CreateComputer([FromBody] ComputerCreationDto input)
+        {
+            var computer = _mapper.Map<Computer>(input);
+            var computerCount = await _repositoryContext.Computers.CountAsync();
+            computer.Name = $"PC{computerCount + 1}";
+
+            _repositoryContext.Computers.Add(computer);
+            await _repositoryContext.SaveChangesAsync();
+
+            var computerDto = _mapper.Map<ComputerDto>(computer);
+
+            return CreatedAtAction("GetComputer", new { computerId = computerDto.Id }, computerDto);
+        }
+
+        [HttpPut("{computerId}")]
+        [ServiceFilter(typeof(ValidationFilterAttribute))]
+        [ServiceFilter(typeof(ValidateComputerExistsAttribute))]
+        public async Task<IActionResult> UpdateComputer(string computerId, [FromBody] ComputerUpdateDto input)
+        {
+            var computer = HttpContext.Items["computer"] as Computer;
+
+            _mapper.Map(input, computer);
+            await _repositoryContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{computerId}")]
+        [ServiceFilter(typeof(ValidateComputerExistsAttribute))]
+        public async Task<IActionResult> DeleteComputer(string computerId)
+        {
+            var computer = HttpContext.Items["computer"] as Computer;
+
+            _repositoryContext.Remove(computer);
+            await _repositoryContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
--- PowerConsumptionAPI/Program.cs
+++ PowerConsumptionAPI/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PowerConsumptionAPI.Filters.ActionFilters;
 using PowerConsumptionAPI.Models;
@@ -7,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
 
 var _connectionString = builder.Configuration.GetConnectionString("sqlConnection");
 
@@ -20,6 +26,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddScoped<ValidationFilterAttribute>();
+builder.Services.AddScoped<ValidateComputerExistsAttribute>();
 
 var app = builder.Build();
 
